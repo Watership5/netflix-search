0790320571a1e4866fa3da2fce1335fc9f303d78
crow <AverageGoku@outlook.com>
2023-08-13 21:31:29 -0500

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import axios from 'axios';
+  const querye =  ref('')
+  const data = ref([]);
+  // console.log(querye);
+  // (async () => {
+  //   try {
+  //     const response = await axios.request(options);
+  //     console.log(response)
+  //     data.value = response.data.titles;
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // })();
+  async function getTitles() {
+    const options = {
+    method: 'GET',
+    url: 'https://netflix54.p.rapidapi.com/search/' + '?nocache=',
+    params: {
+        query: querye.value,
+        offset: '0',
+        limit_titles: '10',
+        limit_suggestions: '10',
+        lang: 'en'
+      },
+      headers: {
+        'X-RapidAPI-Key': 'd8dcd1a415mshd1dab5db95986edp1633e2jsncb045f2bf9bc',
+        'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
+      }
+    };
+    try {
+      const response = await axios.request(options);
+      console.log(response)
+      data.value = response.data.titles;
+      querye.value = ''
+    } catch (error) {
+      console.error(error);
+    }
+  }
+</script>
+<template>
+  <Suspense>
+    <template #default>
+      <main>
+        <input v-model="querye" type="text"/>
+        <h1 v-for="titlea in data" :key="titlea.id">
+          {{ titlea.jawSummary.title }}
+        </h1>
+        <button @click="getTitles">
+          click me
+        </button>
+      </main>
+    </template>
+    <template #fallback>
+      Loading...
+    </template>
+  </Suspense>
+</template>
+
